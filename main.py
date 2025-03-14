413d84dde91e7197fbeb3d5f51df04fab439bada
joyeajin <dpwls31200@gmail.com>
2025-03-14 15:54:59 +0900

    현재 잔고 프린트

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello")
+import ccxt
+
+with open("key/binance_key.txt") as f:
+    lines = f.readlines()
+    api_key = lines[0].strip()
+    secret_key = lines[1].strip()
+
+exchange = ccxt.binance(
+    config={"apiKey": api_key, "secret": secret_key, "enableRateLimit": True}
+)
+
+# balance
+balance = exchange.fetch_balance()
+usdt_balance = balance["USDT"]
+print(usdt_balance["total"])
+
+
+# print("sks")
